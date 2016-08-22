42c1473aa5315326cfe52724c8c362d4e0db4c62
SolidHal <evanmcginnis@ThatMac.local>
2016-08-22 17:12:33 -0500

    Tested Functional, updated latest version from jrnewell/matser

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-    version='3.1',
+    version='2.11',
     packages=find_packages(exclude=["tests"]),
     scripts=['spotify_ripper/main.py'],
     include_package_data=True,
