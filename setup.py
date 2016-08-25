82d5550350d75d61152e7f87c2efcb118e383d83
SolidHal <evanmcginnis@ThatMac.local>
2016-08-25 17:11:57 -0500

    Setup.py updates

--- setup.py
+++ setup.py
@@ -20,9 +20,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-
     version='2.10.5',
-
     packages=find_packages(exclude=["tests"]),
     scripts=['spotify_ripper/main.py'],
     include_package_data=True,
@@ -56,10 +54,8 @@ setup(
                 'to audio files',
     license='MIT',
     keywords="spotify ripper mp3 ogg vorbis flac opus acc mp4 m4a",
-
     url='https://github.com/jrnewell/spotify-ripper',
     download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.10.5',
-
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
