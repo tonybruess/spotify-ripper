324ed8599f40d5ef0519c31a40a3e71f228deab7
SolidHal <evanmcginnis@ThatMac.local>
2016-09-01 18:54:59 -0500

    Removed readme readout in setup.py as it was causing crashes on some system

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
-    long_description=_read('README.rst'),
+    #long_description=_read('README.rst'),
 )
 
 create_default_dir()
