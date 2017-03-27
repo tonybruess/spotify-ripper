96bd924ea8535e8934d27ed5253f7d042fb7c29b
SolidHal <evanmcginnis@ThatMac.local>
2017-03-27 18:53:45 -0500

    Revert "Revert "fixed playlist loading""
    This reverts commit 56cfe7ce5d8e4de93b8d876e99a216858285c8d1.

--- spotify_ripper/remove_all_from_playlist.py
+++ spotify_ripper/remove_all_from_playlist.py
@@ -2,19 +2,28 @@ import spotipy.util as util
 import spotipy.client
 import os
 
+redirect_uri = 'http://www.purple.com'
+client_id = ''
+client_secret = ''
+scope = 'playlist-modify-public playlist-modify-private playlist-read-collaborative'
+
+os.environ["SPOTIPY_CLIENT_ID"] = client_id
+os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
+os.environ["SPOTIPY_REDIRECT_URI"] = redirect_uri
+
 def remove_all_from_playlist(username, playlistURI):
+    tracks = get_playlist_tracks(username, playlistURI)
 
-    redirect_uri = 'http://www.purple.com'
-    client_id = ''
-    client_secret = ''
+    track_ids = []
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        tid = track['id']
+        track_ids.append(tid)
+    results = spotInstance.user_playlist_remove_all_occurrences_of_tracks(username, rPlaylistID, track_ids)
 
-    scope = 'playlist-modify-public playlist-modify-private playlist-read-collaborative'
 
+def get_playlist_tracks(username, playlistURI):
     p1, p2, p3, p4, rPlaylistID = playlistURI.split(':', 5)
-
-    os.environ["SPOTIPY_CLIENT_ID"] = client_id
-    os.environ["SPOTIPY_CLIENT_SECRET"] = client_secret
-    os.environ["SPOTIPY_REDIRECT_URI"] = redirect_uri
 
     token = util.prompt_for_user_token(username, scope)
 
@@ -24,9 +33,5 @@ def remove_all_from_playlist(username, playlistURI):
     results = spotInstance.user_playlist(username, rPlaylistID, fields="tracks,next")
 
     tracks = results['tracks']
-    track_ids = []
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        tid = track['id']
-        track_ids.append(tid)
-    results = spotInstance.user_playlist_remove_all_occurrences_of_tracks(username, rPlaylistID, track_ids)
+
+    return tracks
