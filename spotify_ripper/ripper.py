b1cf4b2cdcd40be3aaaee34ccd40dccd44ba0a66
SolidHal <evanmcginnis@ThatMac.local>
2017-03-28 19:19:38 -0500

    even less verbose

--- spotify_ripper/ripper.py
+++ spotify_ripper/ripper.py
@@ -429,9 +429,6 @@ class Ripper(threading.Thread):
         elif link.type == spotify.LinkType.PLAYLIST:
             print('get playlist tracks')
             self.playlist_uri = uri
-            print(self.playlist_uri)
-            print(type(self.playlist_uri))
-            print('type^')
             tracks = get_playlist_tracks(self.session.user.canonical_name, uri)
             track_list = tracks.get('items')
             for n in track_list:
